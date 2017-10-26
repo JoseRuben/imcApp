140588f72f1b936694e1d3313241c76350b82525
José Rubén Porfirio Ake Vázquez <14470420@itcampeche.edu.mx>
2017-10-26 16:31:25 -0500

    Versión inicial del proyecto ImcApp. No incluye manejo de eventos.

---
+++ app/src/main/java/com/joseake/android/imcapp/ImcFragment.java
@@ -0,0 +1,28 @@
+package com.joseake.android.imcapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by CASA III on 26/10/2017.
+ */
+
+public class ImcFragment extends Fragment{
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragmento_imc, container, false);
+
+        return v;
+    }
+}
